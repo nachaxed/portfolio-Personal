4aa843ef8884ef2054ac82d89701f5727168d686
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-12-30 10:28:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ function seleccionar(){
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
+    if(distancia_skills >= 100){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[2].classList.add("reactjs");
+        habilidades[3].classList.add("python");
+        habilidades[4].classList.add("mongodb");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
